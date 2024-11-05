5f77305c8cca67d7b7b4ce853c54b6984149d8ac
faaarce <risfararie@gmail.com>
2024-11-05 17:28:48 +0700

    add environment

--- Cyclouse/Core/App/Configurations/Environment.swift
+++ Cyclouse/Core/App/Configurations/Environment.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum Environment {
+    case development
+    case staging
+    case production
+    
+    var baseURL: String {
+        switch self {
+        case .development:
+            return "http://localhost:8080"
+        case .staging:
+            return "https://4f7d-182-253-54-18.ngrok-free.app"
+        case .production:
+            return "your-production-url"
+        }
+    }
+}
